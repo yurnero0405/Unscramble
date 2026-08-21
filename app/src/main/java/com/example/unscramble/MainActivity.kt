e827d3e118dfbb980c2f2205b19bc92f7a10259c
Alan_John <alanblancojr@gmail.com>
2026-08-21 18:29:43 +0800

    Phase 4

--- app/src/main/java/com/example/unscramble/MainActivity.kt
+++ app/src/main/java/com/example/unscramble/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.unit.sp
 import com.example.unscramble.ui.theme.UnscrambleTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -36,15 +37,27 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GameScreen() {
 
-    // Stores what the user types
+    // Stores the user's answer
     var userAnswer by remember {
         mutableStateOf("")
     }
 
-    // The correct answer does not change
-    val correctAnswer = "CAT"
+    // List of words
+    val words = listOf(
+        "CAT",
+        "DOG",
+        "BOOK"
+    )
 
-    // Stores the player's score
+    // Keeps track of the current word
+    var currentWordIndex by remember {
+        mutableStateOf(0)
+    }
+
+    // Gets the current correct answer
+    val correctAnswer = words[currentWordIndex]
+
+    // Stores the score
     var score by remember {
         mutableStateOf(0)
     }
@@ -60,8 +73,9 @@ fun GameScreen() {
             fontSize = 30.sp
         )
 
+        // Displays the current word
         Text(
-            text = "TAC",
+            text = correctAnswer,
             fontSize = 40.sp
         )
 
@@ -82,7 +96,17 @@ fun GameScreen() {
         Button(
             onClick = {
                 if (userAnswer == correctAnswer) {
+
+                    // Increase score
                     score++
+
+                    // Move to the next word if available
+                    if (currentWordIndex < words.size - 1) {
+                        currentWordIndex++
+
+                        // Clear the input field
+                        userAnswer = ""
+                    }
                 }
             }
         ) {
