66a1ab633cee5dd4e23d0ef8aaf737b6650e7374
Alan_John <alanblancojr@gmail.com>
2026-08-21 18:20:21 +0800

    Phase 2

--- app/src/main/java/com/example/unscramble/MainActivity.kt
+++ app/src/main/java/com/example/unscramble/MainActivity.kt
@@ -11,6 +11,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
@@ -20,6 +24,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             UnscrambleTheme {
                 GameScreen()
@@ -30,31 +35,44 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GameScreen() {
+
+    // Phase 2: State variable
+    var userAnswer by remember {
+        mutableStateOf("")
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
         Text(
             text = "UNSCRAMBLE",
             fontSize = 30.sp
         )
+
         Text(
             text = "TAC",
             fontSize = 40.sp
         )
+
         Text(
-            text = "Unscramble the word!",
+            text = "Unscramble the word!"
         )
 
+        // Phase 2: Working Input Field
         OutlinedTextField(
-            value = "",
-            onValueChange = { },
+            value = userAnswer,
+            onValueChange = {
+                userAnswer = it
+            },
             label = {
                 Text("Enter your answer")
             }
         )
 
+        // Submit button will be implemented in Phase 3
         Button(
             onClick = { }
         ) {
